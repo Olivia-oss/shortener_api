6a2075ef212d4ef792352c6429585ed67fbf99db
olivia <11.oly.adame@gmail.com>
2024-03-22 15:37:52 -0600

    updates

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,19 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const baseURL = 'https://leafy-tanuki-49e258.netlify.app';
+
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: /^http:\/\/localhost:5173/,
+    origin: baseURL,
     methods: 'GET,POST',
-    credentials: true,
+    optionsSuccessStatus: 200,
   });
+
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
