ace9f7aaa94f038658a411660329e1a48e222c0b
olivia <11.oly.adame@gmail.com>
2024-09-02 21:18:05 -0600

    added api documentation and route routing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const baseURL = 'https://leafy-tanuki-49e258.netlify.app';
@@ -15,6 +16,16 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Api shortener')
+    .setDescription('Docoment Api')
+    .setVersion('1.0')
+    .addTag('Shoerner')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
